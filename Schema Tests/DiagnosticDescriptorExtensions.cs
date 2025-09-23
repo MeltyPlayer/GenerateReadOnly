22328897c42b4abad45e42877098ea59c463d17f
MeltyPlayer <meltyplayer@gmail.com>
2025-09-22 19:43:55 -0500

    Deleted some more low-hanging fruit.

--- Schema Tests/DiagnosticDescriptorExtensions.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.CodeAnalysis;
-
-
-namespace schema.binary;
-
-internal static class DiagnosticDescriptorExtensions {
-  internal static string ToString(this DiagnosticDescriptor src)
-    => src.MessageFormat.ToString();
-}
